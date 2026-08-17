6d497d72f0a69bef298235b09ed1808373eba1d6
HelnaJoshy <helna.joshy.cs@gmail.com>
2026-08-17 10:56:14 +0530

    Add demo user management command

---
+++ Applications/management/commands/create_demo_user.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    help = "Create a demo user for the Job Application Tracker"
+
+    def handle(self, *args, **kwargs):
+
+        username = "demo"
+        password = "demo12345"
+
+        user, created = User.objects.get_or_create(
+            username=username
+        )
+
+        user.set_password(password)
+        user.save()
+
+        if created:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    "Demo user created successfully!"
+                )
+            )
+        else:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    "Demo user already existed. Password has been reset."
+                )
+            )
